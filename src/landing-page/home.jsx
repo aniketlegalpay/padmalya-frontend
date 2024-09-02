9af36da818afa210b2a2cc4c47f2f967d86a78fa
Aniket Anand <Aniket.Anand@legalpay.in>
2024-09-02 14:11:18 +0530

    build new project

---
+++ src/landing-page/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Home = () => {
+  return (
+    <div className="min-h-screen bg-[#fff]  flex items-center text-center">
+        
+     
+        <h1>Welcome to PadMalya</h1>
+      
+        
+    </div>
+  );
+};
+
+export default Home;
